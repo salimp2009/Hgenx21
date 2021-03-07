a9f47dcde773700efd17cd36100876742f372cc6
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-03-08 01:24:42 +0300

    complete premake5.lua file and created GenerateProjects.bat to run the Premake5Exe file; next is event system

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,7 @@
 workspace "Hgenx"
 	architecture "x64"
+	startproject "Sandbox"
+
 	configurations
 	{
 		"Debug",
@@ -7,11 +9,107 @@ workspace "Hgenx"
 		"Dist"
 	}
 
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "Hgenx"
 	location "HGenx"
 	kind "SharedLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
-	target
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-HG_PLATFORM_WINDOWS;HG_BUILD_DLL;
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+			systemversion "latest"
+
+	defines
+	{
+		"HG_PLATFORM_WINDOWS",
+		"HG_BUILD_DLL"
+	}
+			
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+	}
+
+	filter "configurations:Debug"
+		defines "HG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HG_RELEASE"
+		runtime "Release"
+		optimize "on"	
+
+	filter "configurations:Dist"
+		defines "HG_DIST"
+		runtime "Release"
+		optimize "on"	
+
+
+	project "Sandbox"
+		location "Sandbox"
+		kind "ConsoleApp"
+		language "C++"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"HGenx/vendor/spdlog/include",
+		"Hgenx/src",
+		"Hgenx/vendor"
+	}
+
+	links
+	{
+		"Hgenx"
+	}
+
+	filter "system:windows"
+			systemversion "latest"
+
+	defines
+	{
+		"HG_PLATFORM_WINDOWS",
+	}
+
+	filter "configurations:Debug"
+		defines "HG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HG_RELEASE"
+		runtime "Release"
+		optimize "on"	
+
+	filter "configurations:Dist"
+		defines "HG_DIST"
+		runtime "Release"
+		optimize "on"	
