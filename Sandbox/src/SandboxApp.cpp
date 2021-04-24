917300a6afbbcd1400c2010285cc4c58bd9c2c91
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-04-24 23:14:55 +0300

    added OnImGuiRender() function into Sandbox.cpp and used ImgUİ::Begin() and End function . To do this as example we added IMGUI_API __declspec(dllexporT) and dllimport into Hgenx, ImGui and Sandbox projects; to be able to dynamically use them in Sandbox; this is temporary cause we will switch Hgenx to static lib and use static linking

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,4 +1,6 @@
-#include <Hgenx.h>
+﻿#include <Hgenx.h>
+
+#include <imgui/imgui.h>
 
 
 class ExampleLayer : public Hgenx::Layer
@@ -17,6 +19,22 @@ public:
 			HG_TRACE("Tab key is pressed!");
 		}
 
+	}
+
+	virtual void OnImGuiRender() override
+	{
+		// HomeWork Question : How to get rid of the linking error if below is included; compiles ok but cannot link
+		// Solution FOR DDL; Add IMGUI_API=__declspec(dllexport); in Hazel Preprocessor definition also in ImGui 
+		// Also add to SandBox to import IMGUI_API=__declspec(dllimport); Doing this enables Hazel to get 
+		// this loaded from ImGui since Hazel is a DLL library and Sandbox will get it thru Hazel
+		// Not NEEDED FOR STATIC LIBRARY Version of Hazel
+		/* The linking ERRORs occur because those functions were not used in Hazel and no pointers to functions stored there either use those in Hazel or 
+			or do the above declspec instrinsic to export and import dll function pointer so 
+		*/
+		ImGui::Begin("Sandbox Test");
+		ImGui::Text("Welcome to HGenx 2021!");
+		ImGui::End();
+	
 	}
 
 	
