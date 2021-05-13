14f2e4c488d895e7efea4dd704b21ba0beb0fd97
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-05-14 00:41:54 +0300

    added Shader. h and cpp to create an OpenGL shader in the Renderer folder; will moved to Platform/OpenGL folder; copied the sample code from OpenGl ; will modify https://www.khronos.org/opengl/wiki/Shader_Compilation

---
+++ Hgenx/src/Hgenx/Renderer/Shader.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+//#include <cstdint>
+
+namespace Hgenx
+{
+
+	class Shader
+	{
+	public:
+		Shader() = default;
+		/* TODO: will be refactored to receive a filename rather than the string!!*/
+		Shader(const std::string& vertexSrc, std::string& fragmentSrc);
+
+		~Shader();
+
+		void Bind() const;
+		void UnBind() const;
+
+	private:
+		std::uint32_t m_RendererID;
+
+	};
+
+
+
+
+
+
+
+
+} // end of namespace
