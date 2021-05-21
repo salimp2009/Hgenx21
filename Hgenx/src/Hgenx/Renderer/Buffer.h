b709c8cd761ab2f79101d205ce517797cbef327a
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-05-22 00:22:29 +0300

    added pure virtual functions for Vertex and IndexBuffer; non-pure function were not implemented yet

--- Hgenx/src/Hgenx/Renderer/Buffer.h
+++ Hgenx/src/Hgenx/Renderer/Buffer.h
@@ -4,12 +4,34 @@ namespace Hgenx
 {
 	class VertexBuffer
 	{
+	public:
+		virtual ~VertexBuffer() = default;
+
+		virtual void Bind() const = 0;
+		virtual void UnBind() const = 0;
+
+		/*TODO; will be implemented */
+		//virtual const BufferLayout& GetLayout() const& = 0;
+		//virtual void SetLayout(const BufferLayout& layout) = 0;
+
+		/*TODO; Will be implemented*/
+		static VertexBuffer* Create(float* vertices, uint32_t size);
+		//static Ref<VertexBuffer> Create(float* vertices, uint32_t size);
 
 	};
 
 	class IndexBuffer
 	{
+		virtual ~IndexBuffer() = default;
 
+		virtual void Bind() const = 0;
+		virtual void UnBind() const = 0;
+
+		virtual uint32_t GetCount() const = 0;
+
+		/*TODO; Will be implemented*/
+		static IndexBuffer* Create(uint32_t* indices, uint32_t count);
+		//static Ref<IndexBuffer> Create(uint32_t* indices, uint32_t count);  //TODO: Change above to Ref<> (shared pointer)
 
 	};
 
