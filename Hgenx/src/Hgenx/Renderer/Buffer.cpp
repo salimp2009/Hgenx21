41bde8e225db7658906bb4e81f786d56a5ef4439
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-05-22 22:27:09 +0300

    started implemeted Vertex and Index Buffer create function; will add OpenGLVertex and IndexBuffer class that inherits; later will add renderer API to stich types based on which is used in the future (Vulkan or DirectX)

--- Hgenx/src/Hgenx/Renderer/Buffer.cpp
+++ Hgenx/src/Hgenx/Renderer/Buffer.cpp
@@ -5,9 +5,14 @@
 namespace Hgenx
 {
 
+	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	{
+		return nullptr;
+	}
 
-
-
-
+	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t count)
+	{
+		return nullptr;
+	}
 
 }
