58c747533e8031de073e3a2b13c2ab7cba48ee7f
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-05-29 22:40:34 +0300

    added Renderer::GetAPI() and switch statement in the base Buffer class Create function;

--- Hgenx/src/Hgenx/Renderer/Buffer.cpp
+++ Hgenx/src/Hgenx/Renderer/Buffer.cpp
@@ -2,6 +2,7 @@
 #include "Hgenx/Renderer/Buffer.h"
 
 #include "Platform/OpenGL/OpenGLBuffer.h"
+#include "Hgenx/Renderer/Renderer.h"
 
 
 namespace Hgenx
@@ -9,12 +10,31 @@ namespace Hgenx
 
 	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
 	{
-		return new OpenGLVertexBuffer(vertices, size);
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::None:		HG_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
+			case RendererAPI::OpenGL:	return new OpenGLVertexBuffer(vertices, size);
+			
+			/*TODO::*/
+			//case RendererAPI::OpenGL:	return CreateRef<OpenGLVertexBuffer>(vertices, size);  // NOTE: if used change the return type Ref<VertexBuffer>
+			/* ADD more RendererAPI for Vulkan and DirectX*/
+		}
+		HG_CORE_ASSERT(false, "Unkown Renderer!")
+		return nullptr;
 	}
 
 	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t count)
 	{
-		return new OpenGLIndexBuffer(indices, count);
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::None:		HG_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::OpenGL:	return new OpenGLIndexBuffer(indices, count);
+			/*TODO::*/
+			//case RendererAPI::OpenGL:	return CreateRef<OpenGLIndexBuffer>(indices, count); // NOTE: if used change the return type Ref<IndexBuffer>
+			/* ADD more RendererAPI for Vulkan and DirectX*/
+		}
+		HG_CORE_ASSERT(false, "Unkown Renderer!")
+		return nullptr;
 	}
 
 }
